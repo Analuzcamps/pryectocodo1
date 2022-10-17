4036638a51c246d70bb4a4a19e2fd2717049de5d
Analuzcamps <anacoderhouse@gmail.com>
2022-10-17 01:00:30 -0200

    agregamos section nosotros con eror

--- js/main.js
+++ js/main.js
@@ -1,12 +1,45 @@
+
+
+/* Consumiendo API REST
+
+const APIURL = 'https://randomuser.me/api/'
+
+const xhr= new XMLHttpRequest()
+
+function onRequestHandler(){
+
+if (this.readyState == 4 && this.status ==200){
+
+    const data= JSON.parse(this.response);
+    const HTMLResponse= document.querySelector("#sectionNosotros");
+    const tpl = data.map((user) => <li>$[user.name]$[user.picture] </li>);
+HTMLResponse.innerHTML= <ul> ${tpl}</ul>;
+
+}
+}
+
+xhr.addEventListener("load", onRequestHandler);
+xhr.open("GET", APIURL);
+xhr.send();
+
+*/
+
+/* consumienDO API */
+
+function obtenerDatos() {
+    const URLGET = 'https://randomuser.me/api/' ;
+    $.get (URLGET).done(function(resultado, estado) {
+        console.log("el estado que retorna al api es: " + estado);
+        if (estado== "success") {
+            let usuarios= resultado.users;
+            usuarios.forEach(user => {
+                $("#usuarios").append("<tr><td>" + user.name + "</td> <td><img src" + user.picture +"></td></tr>");
+            });
+        }
+    });
+}
+
 //Animaciones con jquery para nav 
-
-$("#animar1").click(function() {
-    console.log("Click en home");
-    $('html').animate({
-        scrollTop: $("#sectionHome").offset().top
-    }, 2000);
-
-});
 
 $("#animar2").click(function() {
     console.log("Click en nosotros");
